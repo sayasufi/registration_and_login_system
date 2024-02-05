a6565d6cde306f19e30672e3bf5a631b40ad66fb
semyon <semyon.ryaboff@yandex.ru>
2024-02-05 18:42:28 +0300

    Reg form

--- source/login/urls.py
+++ source/login/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.Home.as_view(), name="home"),
     path("login/", views.LoginUser.as_view(), name="login"),
     path("logout/", views.logout_view, name="logout"),
+    path("register/", views.RegisterUser.as_view(), name="register"),
 ]
--- source/login/views.py
+++ source/login/views.py
@@ -2,9 +2,9 @@ from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponseNotFound
 from django.shortcuts import redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
-from login.forms import LoginUserForm
+from login.forms import LoginUserForm, RegisterUserForm
 
 
 class Home(ListView):
@@ -27,6 +27,13 @@ class LoginUser(LoginView):
 def logout_view(request):
     logout(request)
     return redirect("home")
+
+
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = "login/register.html"
+    extra_context = {"title": "Регистрация"}
+    # success_url = reverse_lazy("login")
 
 
 def page_not_found(request, exception):
